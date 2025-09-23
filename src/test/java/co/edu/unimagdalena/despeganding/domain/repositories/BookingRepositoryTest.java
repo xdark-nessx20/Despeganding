84197072f612b66fbeab10ae19b080c499945daa
Nestor <nisaumeth@unimagdalena.edu.co>
2025-09-23 13:58:26 -0500

    A falta de implmentar los test method de FlightRepository.

--- src/test/java/co/edu/unimagdalena/despeganding/domain/repositories/BookingRepositoryTest.java
+++ src/test/java/co/edu/unimagdalena/despeganding/domain/repositories/BookingRepositoryTest.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Pageable;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
+import java.util.Comparator;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -24,52 +25,46 @@ class BookingRepositoryTest extends AbstractRepository{
     @BeforeEach
     void setUp() {
         //Given
-        var passenger = Passenger.builder()
-                .fullName("Andres Rudas")
-                .email("rudas@example.com").build();
-        var passenger_2 = Passenger.builder()
-                .fullName("Nestor Saumeth")
-                .email("nsaumeth@example.com").build();
+        var passenger = Passenger.builder().fullName("Andres Rudas").email("rudas@example.com").build();
+        var passenger_2 = Passenger.builder().fullName("Nestor Saumeth").email("nsaumeth@example.com").build();
 
-        var airline = Airline.builder()
-                .code("XD").name("despegar.com").build();
+        var airline = Airline.builder().code("XD").name("despegar.com").build();
 
-        var origin_airport = Airport.builder()
-                .code("SMA").name("despeganding").city("Santa Marta").build();
+        var origin_airport = Airport.builder().code("SMA").name("despeganding").city("Santa Marta").build();
 
-        var destination_airport = Airport.builder()
-                .code("BOG").name("EL DORADO").city("Bogota").build();
+        var destination_airport = Airport.builder().code("BOG").name("EL DORADO").city("Bogota").build();
 
-        var booking = Booking.builder()
-                .createdAt(now.minusDays(3)).updatedAt(now).passenger(passenger).build();
+        var booking = Booking.builder().createdAt(now.minusDays(3)).updatedAt(now).passenger(passenger).build();
 
-        var booking_2 = Booking.builder()
-                .createdAt(now.minusDays(2)).updatedAt(now).passenger(passenger_2).build();
+        var booking_2 = Booking.builder().createdAt(now.minusDays(2)).updatedAt(now).passenger(passenger_2).build();
+        var booking_3 = Booking.builder().createdAt(now.minusDays(10)).updatedAt(now.minusDays(1)).passenger(passenger_2).build();
 
         var flight = Flight.builder()
-                .airline(airline).origin(origin_airport).destination(destination_airport)
-                .departureTime(now.plusHours(1)).arrivalTime(now.plusHours(3)).build();
+            .airline(airline).origin(origin_airport).destination(destination_airport)
+            .departureTime(now.plusHours(1)).arrivalTime(now.plusHours(3)).build();
 
         var booking_item_1 = BookingItem.builder()
-                .cabin(Cabin.ECONOMY).price(new BigDecimal("478000.00")).segmentOrder(5)
-                .booking(booking).flight(flight).build();
+            .cabin(Cabin.ECONOMY).price(new BigDecimal("478000.00")).segmentOrder(5)
+            .booking(booking).flight(flight).build();
         var booking_item_2 = BookingItem.builder()
-                .cabin(Cabin.BUSINESS).price(new BigDecimal("1478000.00")).segmentOrder(5)
-                .booking(booking).flight(flight).build();
-        var booking_item_3 = BookingItem.builder()
-                .cabin(Cabin.BUSINESS).price(new BigDecimal("1478000.00")).segmentOrder(5)
-                .booking(booking_2).flight(flight).build();
+            .cabin(Cabin.BUSINESS).price(new BigDecimal("1478000.00")).segmentOrder(5)
+            .booking(booking_2).flight(flight).build();
+        var booking_item_3 = BookingItem.builder().cabin(Cabin.PREMIUM).price(new BigDecimal("2100000.00")).segmentOrder(1)
+            .booking(booking_3).flight(flight).build();
 
         testEntityManager.persistAndFlush(passenger);
+        testEntityManager.persistAndFlush(passenger_2);
         testEntityManager.persistAndFlush(airline);
         testEntityManager.persistAndFlush(origin_airport);
         testEntityManager.persistAndFlush(destination_airport);
         testEntityManager.persistAndFlush(flight);
         testEntityManager.persistAndFlush(booking);
         testEntityManager.persistAndFlush(booking_2);
+        testEntityManager.persistAndFlush(booking_3);
         testEntityManager.persistAndFlush(booking_item_1);
         testEntityManager.persistAndFlush(booking_item_2);
         testEntityManager.persistAndFlush(booking_item_3);
+
     }
 
     @Test @DisplayName("Booking: find by createdAt between two dates")
@@ -79,20 +74,23 @@ class BookingRepositoryTest extends AbstractRepository{
 
         //Then
         assertThat(loaded).isNotEmpty();
-        assertThat(loaded.getFirst().getCreatedAt())
-            .isBetween(now.minusDays(4), now.minusDays(1));
-
+        assertThat(loaded).allSatisfy(booking ->
+            assertThat(booking.getCreatedAt())
+            .isBetween(now.minusDays(4), now.minusDays(1))
+        );
     }
 
     @Test  @DisplayName("Booking: find by updatedAt between two dates")
     void shouldFindByUpdatedAtBetween() {
         //When
-        var loaded = bookingRepository.findByUpdatedAtBetween(now.minusDays(2), now.minusDays(1));
+        var loaded = bookingRepository.findByUpdatedAtBetween(now.minusDays(2), now.plusHours(1));
 
         //Then
         assertThat(loaded).isNotEmpty();
-        assertThat(loaded.getFirst().getUpdatedAt())
-                .isBetween(now.minusDays(2), now.minusDays(1));
+        assertThat(loaded).allSatisfy(booking ->
+            assertThat(booking.getUpdatedAt())
+            .isBetween(now.minusDays(2), now.minusDays(1))
+        );
     }
 
     @Test @DisplayName("Booking: find bookings by passenger email ignore case and ordered by created at desc")
@@ -104,6 +102,10 @@ class BookingRepositoryTest extends AbstractRepository{
         //Then
         assertThat(loaded).hasSize(2);
         assertThat(loaded).extracting(booking -> booking.getPassenger().getEmail())
-                .containsOnly("nsaumeth@example.com");
+            .containsOnly("nsaumeth@example.com");
+        //This method verifies orderByCreatedAtDesc
+        assertThat(loaded.getContent())
+            .extracting(Booking::getCreatedAt)
+            .isSortedAccordingTo(Comparator.reverseOrder());
     }
 }
--- src/test/java/co/edu/unimagdalena/despeganding/domain/repositories/FlightRepositoryTest.java
+++ src/test/java/co/edu/unimagdalena/despeganding/domain/repositories/FlightRepositoryTest.java
@@ -1,4 +1,34 @@
 package co.edu.unimagdalena.despeganding.domain.repositories;
 
-public class FlightRepositoryTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+public class FlightRepositoryTest extends AbstractRepository{
+    @Autowired FlightRepository flightRepository;
+    @Autowired private TestEntityManager testEntityManager;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test @DisplayName("Flight: Find flights by airline name")
+    void shouldFindByAirline_Name() {
+    }
+
+    @Test @DisplayName("Flight: find flights by origin and destination airport code and departure time between two dates")
+    void shouldFindByOrigin_CodeAndDestination_CodeAndDepartureTimeBetween() {
+    }
+
+    @Test @DisplayName("Flight: filter flights by departure time between two dates, and optionally, by origin and destination airport")
+    void shouldFilterByDepartureTimeBetweenAndOriginAndDestinationOptional() {
+    }
+
+    @Test @DisplayName("Flight: find flights which has the same tags from a list of tags")
+    void shouldFindByHavingSomeTags() {
+
+    }
 }
