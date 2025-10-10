142f31cbbb86f6fc3f9edae0d41a322d24c13b58
Laura Perez <lauperezhdez27@gmail.com>
2025-10-10 09:54:56 -0500

    Guardando cambios locales antes del pull

--- src/test/java/co/edu/unimagdalena/despeganding/api/BookingControllerTest.java
+++ src/test/java/co/edu/unimagdalena/despeganding/api/BookingControllerTest.java
@@ -1,4 +1,10 @@
 package co.edu.unimagdalena.despeganding.api;
 
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(BookingController.class)
+
 public class BookingControllerTest {
+
 }
